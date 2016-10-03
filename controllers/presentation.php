c0142a67ee21ac67adf52994fc8f92652931004c
Missy Oveson <missy@missy.codes>
2016-10-03 17:14:49 -0600

    Add some changes to presentation type and change the speaker from a post type to a taxonomy

--- controllers/presentation.php
+++ controllers/presentation.php
@@ -4,13 +4,24 @@ class presentation {
 	function convention_magic_presentation(){
 		register_post_type( 'convention_magic_presentation',
 			[
-				'labels' => [
-					'name' => __( 'Presentations' ),
-					'singular_name' => __( 'Presentation' )
+				'labels'      => [
+					'name'          => __( 'Presentations' ),
+					'singular_name' => __( 'Presentation' ),
+					'add_new' => 'Add New Presentation',
+					'add_new_item' => 'Add New Presentation',
+					'edit_item' => 'Edit Presentation',
+					'new_item' => 'New Presentation',
+					'all_items' => 'All Presentations',
+					'view_item' => 'View Presentation',
+					'search_items' => 'Search Presentations',
+					'not_found' => 'No Presentations Found',
+					'not_found_in_trash' => 'No Presentations found in Trash',
+					'menu_name' => 'Presentations'
 				],
-				'public' => true,
+				'public'      => true,
 				'has_archive' => true,
-				'rewrite' => [ 'slug' => 'presentations' ]
+				'supports' => array('title', 'editor', 'excerpt', 'custom-fields','thumbnails', 'page-attributes'),
+				'rewrite'     => [ 'slug' => 'presentations' ]
 			]
 		);
 	}
--- controllers/speaker.php
+++ controllers/speaker.php
@@ -3,16 +3,46 @@
 
 class speaker {
 	function convention_magic_speaker(){
-		register_post_type( 'convention_magic_speaker',
-			[
-				'labels' => [
-					'name' => __( 'Speakers' ),
-					'singular_name' => __( 'Speaker' )
-				],
-				'public' => true,
-				'has_archive' => true,
-				'rewrite' => [ 'slug' => 'speakers' ]
-			]
+		$labels = array(
+			'name'                       => _x( 'Speakers', 'taxonomy general name', 'convention-magic' ),
+			'singular_name'              => _x( 'Speaker', 'taxonomy singular name', 'convention-magic' ),
+			'search_items'               => __( 'Search Speakers', 'convention-magic' ),
+			'popular_items'              => __( 'Popular Speakers', 'convention-magic' ),
+			'all_items'                  => __( 'All Speakers', 'convention-magic' ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit Speaker', 'convention-magic' ),
+			'update_item'                => __( 'Update Speaker', 'convention-magic' ),
+			'add_new_item'               => __( 'Add New Speaker', 'convention-magic' ),
+			'new_item_name'              => __( 'New Speaker Name', 'convention-magic' ),
+			'separate_items_with_commas' => __( 'Separate speakers with commas', 'convention-magic' ),
+			'add_or_remove_items'        => __( 'Add or remove speakers', 'convention-magic' ),
+			'choose_from_most_used'      => __( 'Choose from the most used speakers', 'convention-magic' ),
+			'not_found'                  => __( 'No speakers found.', 'convention-magic' ),
+			'menu_name'                  => __( 'Speakers', 'convention-magic' ),
 		);
+
+		$args = array(
+			'hierarchical'          => false,
+			'labels'                => $labels,
+			'show_ui'               => true,
+			'show_admin_column'     => true,
+			'update_count_callback' => '_update_post_term_count',
+			'query_var'             => true,
+			'rewrite'               => array( 'slug' => 'speakers' ),
+		);
+
+		register_taxonomy( 'speakers', 'events', $args );
+	}
+		// Add term page
+		function convention_magic_taxonomy_add_new_meta_field() {
+		// this will add the custom meta field to the add new term page
+		?>
+		<div class="form-field">
+			<label for="term_meta[custom_term_meta]"><?php _e( 'Image', 'convention-magic' ); ?></label>
+			<input type="text" name="term_meta[custom_term_meta]" id="term_meta[custom_term_meta]" value="">
+			<p class="description"><?php _e( 'Enter a value for this field','convention-magic' ); ?></p>
+		</div>
+		<?php
 	}
 }
