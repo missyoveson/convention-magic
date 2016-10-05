127e0e8ac0d7876c7f0cd7863694b18bf789b60a
Missy Oveson <missy@missy.codes>
2016-10-04 20:34:14 -0600

    Add room taxonomy

---
+++ controllers/room.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class room {
+	function convention_magic_room(){
+		$labels = array(
+			'name'                       => _x( 'Rooms', 'taxonomy general name', 'convention-magic' ),
+			'singular_name'              => _x( 'Room', 'taxonomy singular name', 'convention-magic' ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit Room', 'convention-magic' ),
+			'update_item'                => __( 'Update Room', 'convention-magic' ),
+			'add_new_item'               => __( 'Add New Room', 'convention-magic' ),
+			'new_item_name'              => __( 'New Room Name', 'convention-magic' ),
+			'add_or_remove_items'        => __( 'Add or remove rooms', 'convention-magic' ),
+			'choose_from_most_used'      => __( 'Choose from the most used rooms', 'convention-magic' ),
+			'menu_name'                  => __( 'Rooms', 'convention-magic' ),
+		);
+
+		$args = array(
+			'hierarchical'          => false,
+			'labels'                => $labels,
+			'show_ui'               => true,
+			'show_admin_column'     => true,
+			'update_count_callback' => '_update_post_term_count',
+			'query_var'             => true
+		);
+}
