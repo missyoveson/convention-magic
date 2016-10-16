09c7824265f526e7c0e169a1ff661a340ab97d1c
Missy Oveson <missy@missgeekbunny.com>
2016-10-16 13:04:45 -0600

    add sponsor post type

---
+++ models/sponsor.php
@@ -0,0 +1,37 @@
+<?php
+
+class sponsor
+{
+    public function convention_magic_sponsor(){
+        register_post_type( 'convention_magic_sponsor',
+            [
+                'labels'      => [
+                    'name'          => __( 'Sponsors' ),
+                    'singular_name' => __( 'Sponsor' ),
+                    'add_new' => 'Add New Sponsor',
+                    'add_new_item' => 'Add New Sponsor',
+                    'edit_item' => 'Edit Sponsor',
+                    'new_item' => 'New Sponsor',
+                    'all_items' => 'All Sponsors',
+                    'view_item' => 'View Sponsor',
+                    'search_items' => 'Search Sponsors',
+                    'not_found' => 'No Sponsors Found',
+                    'not_found_in_trash' => 'No Sponsors found in Trash',
+                    'menu_name' => 'Sponsors'
+                ],
+                'taxonomy' => array('category', 'post_tag', 'rooms'),
+                'public'      => true,
+                'has_archive' => true,
+                'menu_position' => 23,
+                'supports' => array('title', 'editor', 'excerpt', 'custom-fields','thumbnails', 'page-attributes'),
+                'rewrite'     => [ 'slug' => 'events' ]
+            ]
+        );
+    }
+    public function sponsor_meta_box(){
+
+    }
+    public function sponsor_save(){
+
+    }
+}
